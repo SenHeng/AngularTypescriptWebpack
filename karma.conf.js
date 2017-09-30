513ee50226c97d85d1ed673118013a6c726757d0
Sen Heng <sen@legendofcode.com>
2017-09-30 18:18:00 +0900

    add: mocha, chai

--- karma.conf.js
+++ karma.conf.js
@@ -13,7 +13,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+    frameworks: ['mocha', 'chai', 'karma-typescript', 'karma-webpack'],
 
 
     // list of files / patterns to load in the browser
@@ -31,6 +31,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'src/**/*.ts': ['karma-typescript'],
     },
 
 
